3262f9552e28fa12213d22030d4fb5542386f9cd
Ahmed Shahid <ahmed.shahid.builds@gmail.com>
2026-07-22 03:34:25 +0500

    Changes in app.py and html files

--- app.py
+++ app.py
@@ -5,22 +5,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def hey_faiqa():
-    return render_template("1-hey-faiqa.html")
+    return render_template("hey-faiqa.html")
 
 
-@app.route("/2-something-funny.html")
+@app.route("/something-funny.html")
 def something_funny():
-    return render_template("2-something-funny.html")
+    return render_template("something-funny.html")
 
 
-@app.route("/3-from-the-moment.html")
+@app.route("/from-the-moment.html")
 def from_the_moment():
-    return render_template("3-from-the-moment.html")
+    return render_template("from-the-moment.html")
 
 
-@app.route("/faiqa-marry-me.html")
+@app.route("/would-u-marry-me.html")
 def marry_me():
-    return render_template("faiqa-marry-me.html")
+    return render_template("would-u-marry-me.html")
 
 
 if __name__ == "__main__":
